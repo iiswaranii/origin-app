0da961d7187353dc2a0b273911937542d6167049
Swaran Singh <swaran@Swarans-MacBook-Air.local>
2026-06-13 01:11:20 +0530

    Setup Express backend structure

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const authRoute = require('./routes/auth.routes');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+    res.json({
+        success: true,
+        message: "Origin backend running!"
+    })
+});
+
+app.use("/api/auth", authRoute);
+
+module.exports = app;
